529a7588bd1a47b5eb375153be71b36f86a77415
k5md <serg.dfc@yandex.ru>
2019-03-13 08:09:31 -0400

    implemented stage 2 functionality

--- src/RSSReader.js
+++ src/RSSReader.js
@@ -1,93 +1,161 @@
 import axios from 'axios';
 import { watch } from 'melanke-watchjs';
-
+import { isURL } from 'validator';
 import parse from './parseRSS';
 
 const corsProxy = 'https://cors-anywhere.herokuapp.com/';
 
 const RSSReader = () => {
   const state = {
+    loading: false,
     URL: '',
     URLValid: false,
-    UILoadButtonDisabled: false,
     feeds: [],
     articles: [],
+    error: '',
   };
 
-  const validate = () => true;
+  const validate = (str) => {
+    if (
+      str.length === 0
+      || !isURL(str)
+      || state.feeds.findIndex(feed => feed.URL === str) !== -1
+    ) {
+      return false;
+    }
+    return true;
+  };
 
-  const addFeedButtonHandler = () => {
-    const url = `${corsProxy}${state.URL}`;
-    console.log('fetching', url);
+  const addFeedButtonHandler = (e) => {
+    e.preventDefault();
+    const link = state.URL;
+    const url = `${corsProxy}${link}`;
+    state.loading = true;
 
     axios.get(url, { headers: { 'Access-Control-Allow-Origin': '*' } })
       .then(({ data }) => {
         const { feed, articles } = parse(data);
-        state.feeds = [...state.feeds, feed];
+
+        state.feeds = [...state.feeds, { ...feed, URL: link }];
         state.articles = [...state.articles, ...articles];
-        console.log('fetched', feed, articles, state);
+        state.URL = '';
+      })
+      .catch(() => {
+        state.error = 'Could not fetch data';
+      })
+      .finally(() => {
+        state.loading = false;
       });
   };
 
   const addFeedTextHandler = () => {
     const URL = document.querySelector('#input_text_feed_add').value;
+    const URLValid = validate(URL);
 
-    const UILoadButtonDisabled = validate(state.URL);
     state.URL = URL;
-    state.UILoadButtonDisabled = UILoadButtonDisabled;
-    console.log(URL, 'in handler url', state);
+    state.URLValid = URLValid;
+  };
+
+  const closeErrorAlertHandler = () => {
+    state.error = '';
+  };
+
+  const renderLoadButton = () => {
+    const loadButton = document.querySelector('#input_button_feed_add');
+    loadButton.innerHTML = '';
+    loadButton.textContent = 'Add';
+    loadButton.disabled = !state.URLValid;
+
+    if (state.loading) {
+      const spinner = document.createElement('span');
+      spinner.classList.add('spinner-border');
+      spinner.classList.add('spinner-border-sm');
+      spinner.role = 'status';
+      loadButton.textContent = 'Loading...';
+      loadButton.appendChild(spinner);
+    }
+  };
+
+  const renderFeeds = () => {
+    const feedsList = document.querySelector('#feeds_list');
+    feedsList.innerHTML = '';
+
+    state.feeds.forEach((feed) => {
+      const feedsListItem = document.createElement('li');
+      feedsListItem.classList.add('list-group-item');
+
+      const title = document.createElement('div');
+      title.textContent = feed.title;
+      const description = document.createElement('div');
+      description.textContent = feed.description;
+
+      feedsListItem.appendChild(title);
+      feedsListItem.appendChild(description);
+
+      feedsList.appendChild(feedsListItem);
+    });
+  };
+
+  const renderArticles = () => {
+    const articlesList = document.querySelector('#articles_list');
+    articlesList.innerHTML = '';
+
+    state.articles.forEach((article) => {
+      const articlesListItem = document.createElement('li');
+      articlesListItem.classList.add('list-group-item');
+      const link = document.createElement('a');
+      link.textContent = article.title;
+      link.href = article.link;
+
+      articlesListItem.appendChild(link);
+
+      articlesList.appendChild(articlesListItem);
+    });
+  };
+
+  const renderError = () => {
+    const errorContainer = document.querySelector('#error_container');
+    errorContainer.innerHTML = '';
+
+    if (!state.error) {
+      return;
+    }
+
+    const error = document.createElement('div');
+
+    error.classList.add('alert');
+    error.classList.add('alert-danger');
+    error.classList.add('alert-dismissible');
+    error.role = 'alert';
+    error.textContent = state.error;
+    error.addEventListener('click', closeErrorAlertHandler);
+
+    errorContainer.appendChild(error);
   };
 
   const mount = () => {
     const addFeedText = document.querySelector('#input_text_feed_add');
     const addFeedButton = document.querySelector('#input_button_feed_add');
 
-    const feedsList = document.querySelector('#feeds_list');
-    const articlesList = document.querySelector('#articles_list');
-
-    addFeedText.addEventListener('input', () => addFeedTextHandler());
-    addFeedButton.addEventListener('click', () => addFeedButtonHandler());
+    addFeedText.addEventListener('keyup', addFeedTextHandler);
+    addFeedButton.addEventListener('click', addFeedButtonHandler);
 
     // view section
 
-    watch(state, 'URL', () => {
-      console.log('url changed');
-      addFeedText.value = state.URL;
-    });
-
     watch(state, 'feeds', () => {
-      console.log('feeds changed');
-      state.feeds.forEach((feed) => {
-        console.log(feed);
-        const feedsListItem = document.createElement('li');
-        feedsListItem.classList.add('list-group-item');
-        const title = document.createElement('div');
-        title.textContent = feed.title;
-
-        const description = document.createElement('div');
-        description.textContent = feed.description;
-
-        feedsListItem.appendChild(title);
-        feedsListItem.appendChild(description);
-
-        feedsList.appendChild(feedsListItem);
-      });
+      renderFeeds();
     });
 
     watch(state, 'articles', () => {
-      console.log('articles changed');
-      state.articles.forEach((article) => {
-        console.log(article);
-        const articlesListItem = document.createElement('li');
-        articlesListItem.classList.add('list-group-item');
-        const link = document.createElement('a');
-        link.textContent = article.title;
-        link.href = article.link;
+      renderArticles();
+    });
 
-        articlesListItem.appendChild(link);
+    watch(state, ['URLValid', 'loading'], () => {
+      renderLoadButton();
+    });
 
-        articlesList.appendChild(articlesListItem);
-      });
+    watch(state, 'error', () => {
+      renderError();
     });
   };
 
